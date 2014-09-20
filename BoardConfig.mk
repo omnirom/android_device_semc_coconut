0595dd31e91573a5f24a1e168be2b8cbbe1ef452
Michael Bestas <mikeioannina@gmail.com>
2014-09-20 18:22:51 +0300

    coconut: Use correct flag to disable stereo recording again
    * For some reason coconut doesn't like stereo recording/secondary mic
    
    Change-Id: Ie2817c989eb483bad26b8806a9b61764ea98e33d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,5 +34,5 @@ TARGET_OTA_ASSERT_DEVICE := WT19a,WT19i,coconut
 # Stereo speaker
 BOARD_USES_STEREO_HW_SPEAKER := true
 
-# No back mic
-BOARD_HAVE_BACK_MIC_CAMCORDER := false
+# Disable stereo recording
+BOARD_HAVE_SEMC_AUDIO := false
